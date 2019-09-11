20988395d8607bfa4d513274aede35bcb502d7db
Allan Ricardo Pasquali <allan_pasquali@hotmail.com>
2019-09-11 11:28:50 -0300

    Aula 11-09

---
+++ Aula 11-09 --1/Aula 11-09 --1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_11_09___1
+{
+    class Program
+    {
+        const int testeConst = 999;
+        
+        static void Main(string[] args)
+        {
+            String testeFora;
+            testeFora = "String";
+            var teste1 = "String";
+            var teste2 = 'C';
+            var teste3 = 3;
+            var teste4 = 4.28;
+            var teste5 = 0x0A;
+            Console.WriteLine(testeConst);
+            Console.ReadKey();
+        }
+    }
+}
