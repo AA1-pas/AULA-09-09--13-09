245415ffe7f15c5b89fe52c600deff63fbb0e335
Allan Ricardo Pasquali <allan_pasquali@hotmail.com>
2019-09-13 17:20:34 -0300

    Commit

--- EXERCICIOS/EXERCICIO7/Program.cs
+++ EXERCICIOS/EXERCICIO7/Program.cs
@@ -11,16 +11,16 @@ namespace EXERCICIO7
 
         static void Main(string[] args)
         {
-            string text = string.Empty;
+            
             string nomeCompleto = string.Empty;
             Console.WriteLine("*****************************");
             Console.WriteLine("***** Seja bem Vindo!!! *****");
             Console.WriteLine("*****************************\n\n");
             Console.WriteLine("Digite seu texto: ");
-            text = Console.ReadLine();
+            string text = Console.ReadLine();
             Console.WriteLine("\n");
-            Console.WriteLine($@"Esse texto possue {ContaVogais("a", "A", text)} vogai(s) 'a', {ContaVogais("e", "E" , text)} vogai(s) 'e', 
-{ContaVogais("i", "I", text)} vogai(s) 'i', {ContaVogais("o", "O", text)} vogai(s) 'o' e {ContaVogais("u","U", text)} vogai(s) 'u'. ");
+            Console.WriteLine($@"Esse texto possue {ContaVogais("a", "A", text)} vogai(s) 'A', {ContaVogais("e", "E" , text)} vogai(s) 'E', 
+{ContaVogais("i", "I", text)} vogai(s) 'I', {ContaVogais("o", "O", text)} vogai(s) 'O' e {ContaVogais("u","U", text)} vogai(s) 'U'. ");
             Console.WriteLine("\n\nAperte qualquer tecla para sair.");
             Console.ReadKey();
         }
@@ -34,10 +34,10 @@ namespace EXERCICIO7
         private static int ContaVogais(string vogal, string vogalMaiu, string textFind)
         {
             string[] stringFind = new string[] { vogal };
-            string[] firstNames = textFind.Split(stringFind, StringSplitOptions.None);
+            string[] quebraMin = textFind.Split(stringFind, StringSplitOptions.None);
             string[] stringFind2 = new string[] { vogalMaiu };
-            string[] firstNames2 = textFind.Split(stringFind2, StringSplitOptions.None);
-            var numberFind = firstNames.Length - 1+(firstNames2.Length - 1);
+            string[] quebraMaiu= textFind.Split(stringFind2, StringSplitOptions.None);
+            var numberFind = quebraMin.Length - 1+(quebraMaiu.Length - 1);
             return numberFind;
 
         }
---
+++ FOREACH/FOREACH/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOREACH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>() { "John", "Tom", "Peter" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
