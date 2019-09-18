060f0ce5f835f91843e25bf3bf0cecdf1fdfa8e2
Victor <Victor@DESKTOP-CH73UP4>
2019-09-17 22:17:31 -0300

    Commit

--- EXERCICIOS1709/PROJETOEXERCICIO1709/Program.cs
+++ EXERCICIOS1709/PROJETOEXERCICIO1709/Program.cs
@@ -10,26 +10,115 @@ namespace PROJETOEXERCICIO1709
     {
         static void Main(string[] args)
         {
-            DesenhoArvore();
+            MenuPrincipal();
         }
 
-        public static void DesenhoArvore()
+        /// <summary>
+        /// Metodo de menu principal de escolha dos metodos
+        /// </summary>
+        public static void MenuPrincipal()
         {
-            for (int i = 0; i < 8; i++)
+            Console.Clear();
+            Console.WriteLine(@"Qual método você deseja executar?
+1-Árvore  
+2-Área Quadrado 
+3-Lista Cerveja 
+4-Lista Marca Carro 
+
+0-Sair
+");
+            switch(Console.ReadKey().KeyChar.ToString())
             {
-                int espaco = 10;
-                for (int j = 0; j < espaco; j++)
-                {
-                    if (j%2 == 0)
-                    Console.Write(" ");
-                    else
-                    Console.Write("     ");
-                }
-                Console.Write("*");
-                Console.Write("\n"); 
+                case "1":
+                    Console.Clear();
+                    DesenhandoArvore();
+                    break;
+                case "2":
+                    Console.Clear();
+                    AreaQuadrado();
+                    break;
+                case "3":
+                    Console.Clear();
+                    DesenhandoArvore();
+                    break;
+                case "4":
+                    Console.Clear();
+                    DesenhandoArvore();
+                    break;
+                case "0":
+                    break;
+                default:
+                    Console.Clear();
+                    RetornaMenu("*** OPÇÃO INVÁLIDA! ***\n\n");
+                    break;
 
             }
+            
+        }
+
+ 
+        /// <summary>
+        /// Metodo desenha cada linha da arvore
+        /// </summary>
+        /// <param name="espacador">recebe quantos espacos deve imprimir</param>
+        private static void EspacoArvore(double espacador)
+        {
+            for (int i = 0; i < espacador; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        /// <summary>
+        /// metodo contem os dados e desenha a arvore
+        /// </summary>
+        private static void DesenhandoArvore()
+        {
+            List<int> formatoArvore = new List<int>
+            { 4,9,8,14,12,16,13,17,14,15,12,13,10,10,4,4,4,4,4,4,4,4,5,6,8,13,40};
+
+            foreach (var i in formatoArvore)       
+            {
+                EspacoArvore(40-i);
+                DesenhandoALinhaArvore(2*i);
+                Console.Write("\n");
+            }
+
+            RetornaMenu("");
+        }
+
+        /// <summary>
+        /// Retorna mesagem de erro se tiver e volta ao menu principal ao apertar qualquer tecla
+        /// </summary>
+        /// <param name="menssagemErro">menssagem de advertencia ao usuário</param>
+        private static void RetornaMenu(string menssagemErro)
+        {
+            Console.WriteLine(menssagemErro + "Tecle qualquer tecla para retornar ao menu principal.");
             Console.ReadKey();
+            MenuPrincipal();
+        }
+        
+        /// <summary>
+        /// Metodo desenha cada linha da arvore
+        /// </summary>
+        /// <param name="espacador">recebe quantos caracteres deve imprimir</param>
+        private static void DesenhandoALinhaArvore(int espacador)
+        {
+            for (int i = 0; i < espacador; i++)
+            {
+                Console.Write(".");
+            }
+        }
+
+        /// <summary>
+        /// Metodo calcula área quadrado
+        /// </summary>
+        private static void AreaQuadrado()
+        {
+            Console.WriteLine("Digite a medida do lado do quadrado (cm).");
+            double ladoQuadrado = double.Parse((Console.ReadLine()));
+            Console.WriteLine("\n"+$"A área do quadrado é de {ladoQuadrado* ladoQuadrado} cm²."+"\n");
+            RetornaMenu("");
         }
     }
 }
