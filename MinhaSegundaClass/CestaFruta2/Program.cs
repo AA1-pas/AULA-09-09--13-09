97eae00b6356c9833c87b0ca7eb7160f418d1b9e
Allan Ricardo Pasquali <allan_pasquali@hotmail.com>
2019-09-18 17:28:46 -0300

    commit

---
+++ MinhaSegundaClass/CestaFruta2/Program.cs
@@ -0,0 +1,29 @@
+﻿using CestaFruta2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestaDeFrutas = new List<MinhasFrutas>();
+            {
+                minhaCestaDeFrutas.Add(new Fr()
+                {
+                    Banana = 6,
+                    Laranja = 10,
+                    Tomate = 3
+                });
+
+                listaDeCestas.ForEach(i =>
+                Console.WriteLine($"Banana {i.Banana} tomate {i.Tomate} laranja {i.Laranja}"));
+                Console.ReadKey();
+            }
+        }
+    }
+}
